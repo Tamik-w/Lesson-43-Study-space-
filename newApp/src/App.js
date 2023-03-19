709a903f5d32efab254a591a2501501849c77932
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-03-20 02:43:02 +0300

    templates created

---
+++ newApp/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Article from "./components/Article";
+
+function App() {
+  return (
+    <div className="wrapper">
+
+    </div> 
+    );
+  }
+export default App;
